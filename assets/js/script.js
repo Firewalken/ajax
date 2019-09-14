68a2eb6ef452e2fd859af3f7fe9bbe6782937fcb
No_Name <miniganno1@mail.ru>
2019-09-15 05:39:11 +0700

    homework

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,12 @@ ajaxButton.addEventListener('click', sendRequest)
 
 
 function sendRequest(params) {
-    console.log('кликнули на кнопку');
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            // document.getElementById("demo").innerHTML = this.responseText;
-            console.log("пришел ответ от сервер")
-            // console.log(this.responseText)
+            console.log("пришел ответ от сервера")
             if (document.getElementById("ajaxTable") === null) {
-                ajaxInsert(this.responseText);
+                hbsAjaxInsert(this.responseText);
             } else {
                 alert("Таблица уже создана");
             }
@@ -25,12 +22,12 @@ function sendRequest(params) {
             console.log("this.status: " + this.status);
         }
     };
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/photos", true);
     xhr.send();
 }
 
 
-function ajaxInsert(data) {
+function nativeJS_ajaxInsert(data) {
     let result = JSON.parse(data);
 
     // 1. Создаём таблицу
@@ -39,7 +36,7 @@ function ajaxInsert(data) {
         id: "ajaxTable",
         place: ajaxResultsWrapper
     }
-    createEl(resultTable); // Создаем таблица
+    createEl(resultTable); // Создаем таблицу
 
     result.forEach( (element, index) => {
         console.log(element)
@@ -69,6 +66,42 @@ function ajaxInsert(data) {
     });
 }
 
+function hbsAjaxInsert(ajaxResponse) {
+    
+    let result = JSON.parse(ajaxResponse);
+
+    // сделать рандомную выборку картинок из массива, в диапазоне 5
+
+    rangeValue = +prompt("Укажите размер диапазона", 5);
+    console.log(rangeValue);
+
+    if (rangeValue > result.length || rangeValue < 5 || !Boolean(rangeValue)) {   // проверка допустимости диапазона
+        alert(`Выборка невозможна так как выбранный диапазон не соответствует размерам массива, либо не является числом`);
+        return;
+    }
+
+    let rand;
+
+    for (let i = 0; i < result.length; i++) {   // поиск первого подходящего случайного числа для старта выборки
+        rand = Math.floor(Math.random() * result.length);
+        if (rand <= result.length - rangeValue && rand >= 0) break;
+    }
+
+
+    let newArr = result.slice(rand, rand + rangeValue);    // выборка от случайного элемента в массиве по указанному диапазону
+    console.log(newArr.length);
+
+
+    var ajaxTableHTML = document.querySelector("#ajax_table_hbs").innerHTML; // хранит HTML в строке
+    var ajaxTableTemplate = Handlebars.compile(ajaxTableHTML); // содержит собранный handlebars-шаблон из source
+
+    var context = { rows: newArr, author: "Илья"}; // информация которую необходимо записать в шаблон
+    var readyTableHTML = ajaxTableTemplate(context); // выдает собранный html в текстовом виде
+
+    document.body.innerHTML = readyTableHTML;
+
+}
+
 function createEl(obj) {
     let newItem = document.createElement(obj.tag);
     newItem.setAttribute("id", obj.id);
@@ -77,10 +110,24 @@ function createEl(obj) {
 }
 
 
-function createElOLD(tagName, innerContent, place, id) {
-    let newItem = document.createElement(tagName);
-    newItem.textContent = innerContent;
-    place.appendChild(newItem);
-}
+// function createElOLD(tagName, innerContent, place, id) {
+//     let newItem = document.createElement(tagName);
+//     newItem.textContent = innerContent;
+//     place.appendChild(newItem);
+// }
 
 
+
+
+// var source   = $("#entry-template").html();
+// var source   = document.querySelector("#entry-template").innerHTML; // хранит HTML в строке
+// var template = Handlebars.compile(source); // содержит собранный handlebars-шаблон из source
+
+// var context = { title: "Собаке Качалова", someinfo: "haha" }; // информация которую необходимо записать в шаблон
+// var html    = template(context); // выдает собранный html в текстовом виде
+
+// console.log(html);
+
+
+
+// MVC - MODEL - VIEWER - CONTROLLER
